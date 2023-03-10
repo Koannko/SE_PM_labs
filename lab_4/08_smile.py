efecced692ae043f05f7871c4c8a883501c78bf5
Koannko <kostyleva-2000@bk.ru>
2023-03-10 20:11:59 +0500

    lab_4 has been completed

--- lab_4/08_smile.py
+++ lab_4/08_smile.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
+import simple_draw as sd
+import random
 
 # Написать функцию отрисовки смайлика по заданным координатам
 # Форма рожицы-смайлика на ваше усмотрение
@@ -10,5 +11,16 @@ import simple_draw
 # Вывести 10 смайликов в произвольных точках экрана.
 
 # TODO здесь ваш код
+sd.resolution = (1000, 600)
+def draw_smile(x, y, color):
+    centre = sd.get_point(x, y)
+    sd.circle(center_position=centre, radius=50, color=color, width=50)
+    sd.line(start_point=sd.get_point(x-10,y-25), end_point=sd.get_point(x+10, y-25), color=sd.COLOR_BLACK, width=5)
+    sd.line(start_point=sd.get_point(x-30,y), end_point=sd.get_point(x+-20, y+20), color=sd.COLOR_BLACK, width=5)
+    sd.line(start_point=sd.get_point(x-20,y+20), end_point=sd.get_point(x-10, y), color=sd.COLOR_BLACK, width=5)
+    sd.line(start_point=sd.get_point(x+10,y), end_point=sd.get_point(x+20, y+20), color=sd.COLOR_BLACK, width=5)
+    sd.line(start_point=sd.get_point(x+20,y+20), end_point=sd.get_point(x+30, y), color=sd.COLOR_BLACK, width=5)
 
-simple_draw.pause()
+for i in range(10):
+    draw_smile(sd.random_number(100, 900), sd.random_number(100, 500), sd.random_color())
+sd.pause()
