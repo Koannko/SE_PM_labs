811898cd57323d3b3ca26eae4e7a35c24656e43e
Koannko <kostyleva-2000@bk.ru>
2023-03-17 13:31:34 +0700

    lab_7 has been completed

---
+++ lab_6/try.py
@@ -0,0 +1,55 @@
+from lab6 import num_eights
+
+def pingpong(n):
+    """Возвращает n-ый элемент пинг-понг последовательности.
+
+    Пинг-понг последовательность начинается с 1.
+    Следующий элемент получается прибавлением приращения к предыдущему.
+    Начальное приращение: +1.
+    Если номер элемента кратен 8 или содержит цифру 8 - знак приращения меняется (обозначено *):
+
+    Номер   1	2	3	4	5	6	7	8*	9	10	11	12	13	14	15	16*	17	18*	19	20	21 ...
+
+    Элемент 1	2	3	4	5	6	7	8*	7	 6	 5	 4	 3	 2	 1	 0*	 1	 2*	 1	 0	-1 ...
+
+    >>> pingpong(8)
+    8
+    >>> pingpong(10)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    -2
+    >>> pingpong(30)
+    -2
+    >>> pingpong(68)
+    0
+    >>> pingpong(69)
+    -1
+    >>> pingpong(80)
+    0
+    >>> pingpong(81)
+    1
+    >>> pingpong(82)
+    0
+    >>> pingpong(100)
+    -6
+    >>> from construct_check import check
+    >>> # в этом задании запрещено использовать оператор связывания
+    >>> # используйте только рекурсию
+    >>> check(LAB_SOURCE_FILE, 'pingpong', ['Assign', 'AugAssign'])
+    True
+    """
+    def help_pingpong(value, increment, k):
+        if k == n:
+            return value + increment
+        if k % 8 == 0 or num_eights(k) > 0:
+            return help_pingpong(value + increment, -increment, k + 1)
+        else:
+            return help_pingpong(value + increment, increment, k + 1)
+        
+    return help_pingpong(0, 1, 1)
+
+print(pingpong(21))
