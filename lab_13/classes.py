cc7a518ebcb1f55e6693dadce8ef387c40c0dbd4
Koannko <kostyleva-2000@bk.ru>
2023-05-12 03:49:48 +0700

    complete lab_13

--- lab_13/classes.py
+++ lab_13/classes.py
@@ -1,11 +1,31 @@
 from abc import ABC, abstractmethod
 from datetime import date, timedelta
+from docx import Document
+from docx.enum.table import WD_TABLE_ALIGNMENT
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Cm
+
 import calendar
 
 class BankService(ABC):
-    @abstractmethod
     def calculate_interest(self):
-        pass
+        return round(self.debt * self.interest / 1200, 2)
+    
+    def get_date_of_payment(self):
+        days = calendar.monthrange(self.date.year, self.date.month)[1]
+        self.date += timedelta(days=days)
+        month = calendar.month_abbr[self.date.month]
+        return f'{month} {self.date.day}, {self.date.year}'
+
+    def calculate_overpayments(self):
+        return self.calculate_amount_of_payment() - self.amount
+
+    def pay_reg_payment(self):
+        self.debt -= self.calculate_main_debt()
+        self.current_month += 1
+
+        if round(self.debt) == 0:
+            self.debt = round(self.debt, 1)
     
     @abstractmethod
     def calculate_month_payment(self):
@@ -13,14 +33,6 @@ class BankService(ABC):
 
     @abstractmethod
     def calculate_amount_of_payment(self):
-        pass
-
-    @abstractmethod
-    def calculate_overpayments(self):
-        pass
-
-    @abstractmethod
-    def pay_month_payment(self):
         pass
 
     @abstractmethod
@@ -31,6 +43,10 @@ class BankService(ABC):
     def activate(self):
         pass
 
+    @abstractmethod
+    def save_docx(self):
+        pass
+
 class Credit(BankService):
     def __init__(self, amount, interest, period):
         self.amount = amount
@@ -39,10 +55,7 @@ class Credit(BankService):
         self.debt = amount
         self.date = date.today()
         self.current_month = 0
-
-    def calculate_interest(self):
-        return round(self.debt * self.interest / 1200, 2)
-    
+        
     def calculate_month_payment(self):
         return self.amount * self.interest / \
                 1200 * (1 + self.interest / 1200) ** self.period / \
@@ -51,31 +64,15 @@ class Credit(BankService):
     def calculate_amount_of_payment(self):
         return self.calculate_month_payment() * self.period
     
-    def calculate_overpayments(self):
-        return self.calculate_amount_of_payment() - self.amount
-    
     def calculate_main_debt(self):
         return self.calculate_month_payment() - self.calculate_interest()
     
-    def get_date_of_payment(self):
-        days = calendar.monthrange(self.date.year, self.date.month)[1]
-        self.date += timedelta(days=days)
-        month = calendar.month_abbr[self.date.month]
-        return f'{month} {self.date.day}, {self.date.year}'
-
-    def pay_month_payment(self):
-        self.debt -= self.calculate_main_debt()
-        self.current_month += 1
-
-        if round(self.debt) == 0:
-            self.debt = round(self.debt, 1)
-
     def check_input(self):
         try:
             self.amount = float(self.amount)
             self.interest = float(self.interest)
             self.period = int(self.period) 
-            # self.amount <= 0 or self.interest <= 0 or self.period <= 0
+            self.amount <= 0 or self.interest <= 0 or self.period <= 0
         except:    
             return f'Вы ввели недопустимые данные: {self.amount}, {self.interest}, {self.period}. Кредит не может быть рассчитан.'
         return True
@@ -89,7 +86,7 @@ class Credit(BankService):
                     ['График платежей', self.period]]
         data_list = []
         while self.current_month < self.period:
-            self.pay_month_payment()
+            self.pay_reg_payment()
             data_list.append([['№', str(self.current_month).zfill(2)],
                             ['Дата платежа', self.get_date_of_payment()],
                             ['Платёж в месяц', round(self.calculate_month_payment(), 2)],
@@ -98,6 +95,295 @@ class Credit(BankService):
                             ['Остаток', round(self.debt, 2)]])     
         return data_list, compute_list
         
-def create_credit(amount, interest, period):
-    credit = Credit(amount, interest, period)
-    return credit
+    def save_docx(self, data_lst, copy_num):
+        document = Document()
+        document.add_heading('Кредитный калькулятор', level=0)
+        document.add_heading('Ввeденные данные', level=1)
+        document.add_paragraph(f'Сумма кредита  {self.amount} ₽')
+        document.add_paragraph(f'Процентная ставка  {self.interest} процентов')
+        document.add_paragraph(f'Срок кредитования  {self.period} месяцев')
+        document.add_heading('Результаты расчёта', level=1)
+        document.add_paragraph(f'Ежемесячный платеж  {round(self.calculate_month_payment(), 2)} ₽')
+        document.add_paragraph(f'Переплата по кредиту  {round(self.calculate_overpayments(), 2)} ₽')
+        document.add_heading('График платежей', level=1)
+
+        table = document.add_table(rows=1, cols=6, style='Table Grid')
+        table.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+        column_widths = [Cm(1.0), Cm(3.0), Cm(3.0), Cm(3.0), Cm(3.0), Cm(3.0)]
+        for idx, width in enumerate(column_widths):
+            table.columns[idx].width = width
+
+        hdr_cells = table.rows[0].cells
+        hdr_cells[0].text = '№'
+        hdr_cells[1].text = 'Дата платежа'
+        hdr_cells[2].text = 'Платёж в месяц'
+        hdr_cells[3].text = 'Основной долг'
+        hdr_cells[4].text = 'Проценты'
+        hdr_cells[5].text = 'Остаток'
+
+        for i, row in enumerate(data_lst):
+            table.add_row()
+            for j, val in enumerate(row):
+                cell = table.cell(i+1, j)
+                if j > 1:
+                    cell.text = str(val[1]) + ' ₽'
+                else:
+                    cell.text = str(val[1])
+
+        for row in table.rows:
+            for cell in row.cells:
+                cell.paragraphs[0].alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+        table.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+        document.add_page_break()
+        if copy_num == 0:
+            document.save('credit.docx')
+        else:
+            document.save(f'credit{copy_num}.docx')
+
+
+# РАССРОЧКА
+class Installment(BankService):
+    def __init__(self, amount, interest, period):
+        self.amount = amount
+        self.interest = interest
+        self.period = period
+        self.debt = amount
+        self.date = date.today()
+        self.current_month = 0
+        
+    def calculate_month_payment(self):
+        return self.calculate_interest() + self.amount / self.period
+    
+    def get_days_in_month(self):
+        return calendar.monthrange(self.date.year, self.date.month)[1]
+
+    def get_days_in_year(self):
+        return 365 + calendar.isleap(self.date.year)
+
+    def calculate_amount_of_payment(self):
+        sum = self.amount
+        payments = sum
+        diff = self.amount / self.period
+        for i in range(self.period):
+            payments += self.interest / 1200 * sum
+            sum -= diff
+        return payments
+    
+    def calculate_main_debt(self):
+        return self.amount / self.period
+    
+    def get_current_debt(self):
+        return self.debt - self.calculate_main_debt()
+
+    def check_input(self):
+        try:
+            self.amount = float(self.amount)
+            self.interest = float(self.interest)
+            self.period = int(self.period) 
+            self.amount <= 0 or self.interest <= 0 or self.period <= 0
+        except:    
+            return f'Вы ввели недопустимые данные: {self.amount}, {self.interest}, {self.period}. Рассрочка не может быть рассчитана.'
+        return True
+
+    def activate(self):
+        if self.check_input() != True:
+            return self.check_input()
+        compute_list = [['Средний платёж в месяц', round(self.calculate_amount_of_payment() / self.period, 2)],
+                        ['Переплата', round(self.calculate_overpayments(), 2)]]
+        data_list = []
+        while self.current_month < self.period:
+            data_list.append([['№', str(self.current_month).zfill(2)],
+                            ['Дата платежа', self.get_date_of_payment()],
+                            ['Платёж в месяц', round(self.calculate_month_payment(), 2)],
+                            ['Основной долг', round(self.calculate_main_debt(), 2)],
+                            ['Проценты', round(self.calculate_interest(), 2)],
+                            ['Остаток', round(self.get_current_debt(), 2)]]) 
+            self.pay_reg_payment()
+        return data_list, compute_list
+        
+    def save_docx(self, data_lst, copy_num):
+        document = Document()
+        document.add_heading('Калькулятор рассрочки', level=0)
+        document.add_heading('Ввeденные данные', level=1)
+        document.add_paragraph(f'Сумма покупки  {self.amount} ₽')
+        document.add_paragraph(f'Процентная ставка  {self.interest} процентов')
+        document.add_paragraph(f'Срок рассрочки  {self.period} месяцев')
+        document.add_heading('Результаты расчёта', level=1)
+        document.add_paragraph(f'Платёж в месяц в среднем  {round(self.calculate_amount_of_payment(), 2)} ₽')
+        document.add_paragraph(f'Переплата  {round(self.calculate_overpayments(), 2)} ₽')
+        document.add_heading('График платежей', level=1)
+
+        table = document.add_table(rows=1, cols=6, style='Table Grid')
+        table.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+        column_widths = [Cm(1.0), Cm(3.0), Cm(3.0), Cm(3.0), Cm(3.0), Cm(3.0)]
+        for idx, width in enumerate(column_widths):
+            table.columns[idx].width = width
+
+        hdr_cells = table.rows[0].cells
+        hdr_cells[0].text = '№'
+        hdr_cells[1].text = 'Дата платежа'
+        hdr_cells[2].text = 'Платёж в месяц'
+        hdr_cells[3].text = 'Основной долг'
+        hdr_cells[4].text = 'Проценты'
+        hdr_cells[5].text = 'Остаток'
+
+        for i, row in enumerate(data_lst):
+            table.add_row()
+            for j, val in enumerate(row):
+                cell = table.cell(i+1, j)
+                if j > 1:
+                    cell.text = str(val[1]) + ' ₽'
+                else:
+                    cell.text = str(val[1])
+
+        for row in table.rows:
+            for cell in row.cells:
+                cell.paragraphs[0].alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+        table.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+        document.add_page_break()
+        if copy_num == 0:
+            document.save('installment.docx')
+        else:
+            document.save(f'installment{copy_num}.docx')
+
+
+
+
+
+
+# ВКЛАД
+class Deposit(BankService):
+    def __init__(self, amount, interest, period, interest_type, additions, amount_of_additions):
+        self.amount = amount
+        self.interest = interest
+        self.period = period
+        self.interest_type = interest_type
+        self.additions = additions
+        self.amount_of_additions = amount_of_additions
+        self.date = date.today()
+        self.current_month = 0
+        self.debt = self.amount
+        
+    def calculate_month_payment(self):
+        return self.calculate_interest()
+
+    def calculate_amount_of_payment(self):
+        if self.additions == 'Каждый месяц':
+            return float(self.amount_of_additions) * self.period
+        elif self.additions == 'Каждый квартал':
+            return float(self.amount_of_additions) * (self.current_month // 3)
+        elif self.additions == 'Каждый год':
+            return float(self.amount_of_additions) * (self.current_month // 12)
+        else:
+            return 0
+    
+    def calculate_main_debt(self):
+        return self.amount
+    
+    def calculate_addition(self):
+        if self.current_month == 0: return 0
+
+        if self.additions == 'Каждый месяц':
+            return float(self.amount_of_additions)
+        elif self.additions == 'Каждый квартал' and self.current_month % 3 == 0:
+            return float(self.amount_of_additions)
+        elif self.additions == 'Каждый год' and self.current_month % 12 == 0:
+            return float(self.amount_of_additions)
+        else:
+            return 0
+    
+    def pay_reg_payment(self):
+        self.amount += float(self.calculate_addition())
+        self.current_month += 1
+        
+        if self.interest_type == True:
+            print('YES')
+            self.amount += self.calculate_interest()
+            
+    
+    def check_input(self):
+        try:
+            self.amount = float(self.amount)
+            self.interest = float(self.interest)
+            self.period = int(self.period) 
+            self.amount <= 0 or self.interest <= 0 or self.period <= 0 or self.amount_of_additions == ''
+        except:    
+            return f'Вы ввели недопустимые данные: {self.amount}, {self.interest}, {self.period}. Вклад не может быть рассчитана.'
+        return True
+
+    def activate(self):
+        if self.check_input() != True:
+            return self.check_input()
+        data_list = []
+        compute_list = []
+        while self.current_month < self.period:
+            self.pay_reg_payment()
+            lst = [['№', str(self.current_month).zfill(2)],
+                            ['Дата начисления', self.get_date_of_payment()],
+                            ['Начислено процентов', round(self.calculate_month_payment(), 2)],
+                            ['Пополнение вклада', round(self.calculate_addition(), 2)],
+                            ['Остаток вклада', round(self.calculate_main_debt(), 2)]]
+            if self.current_month == self.period:
+                compute_list.append(['Сумма в конце срока', round(lst[4][1], 2)])
+                compute_list.append(['Доход', round(lst[4][1] - self.debt, 2)])
+            data_list.append(lst) 
+        print(compute_list)   
+        return data_list, compute_list
+        
+    def save_docx(self, data_lst, compute_list, copy_num):
+        if self.interest_type == True: interest_type = 'Да' 
+        else: interest_type = 'Нет'
+        document = Document()
+        document.add_heading('Калькулятор доходности вкладов', level=0)
+        document.add_heading('Ввeденные данные', level=1)
+        document.add_paragraph(f'Сумма вклада  {self.debt} ₽')
+        document.add_paragraph(f'Процентная ставка  {self.interest} процентов')
+        document.add_paragraph(f'Срок размещения  {self.period} месяцев')
+        document.add_paragraph(f'Добавлять начисления ко вкладу  {interest_type}')
+        document.add_paragraph(f'Сумма пополнений вклада  {self.amount_of_additions} ₽')
+        document.add_heading('Результаты расчёта', level=1)
+        document.add_paragraph(f'Сумма в конце срока  {round(compute_list[0][1])} ₽')
+        document.add_paragraph(f'Доход  {round(compute_list[1][1])} ₽')
+        document.add_heading('График начислений', level=1)
+
+        table = document.add_table(rows=1, cols=5, style='Table Grid')
+        table.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+        column_widths = [Cm(1.0), Cm(3.0), Cm(3.0), Cm(3.0), Cm(3.0)]
+        for idx, width in enumerate(column_widths):
+            table.columns[idx].width = width
+
+        hdr_cells = table.rows[0].cells
+        hdr_cells[0].text = '№'
+        hdr_cells[1].text = 'Дата начисления'
+        hdr_cells[2].text = 'Начислено процентов'
+        hdr_cells[3].text = 'Пополнение вклада'
+        hdr_cells[4].text = 'Остаток вклада'
+
+        for i, row in enumerate(data_lst):
+            table.add_row()
+            for j, val in enumerate(row):
+                cell = table.cell(i+1, j)
+                if j > 1:
+                    cell.text = str(val[1]) + ' ₽'
+                else:
+                    cell.text = str(val[1])
+
+        for row in table.rows:
+            for cell in row.cells:
+                cell.paragraphs[0].alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+        table.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+        document.add_page_break()
+        if copy_num == 0:
+            document.save('deposit.docx')
+        else:
+            document.save(f'deposit{copy_num}.docx')
+
--- lab_13/main.py
+++ lab_13/main.py
@@ -4,6 +4,8 @@ from   pyforms.basewidget import BaseWidget
 from   pyforms.controls import ControlText
 from   pyforms.controls import ControlButton
 from   pyforms.controls import ControlTextArea
+from   pyforms.controls import ControlCombo
+from   pyforms.controls import ControlCheckBox
 
 class GUI(BaseWidget):
 
@@ -15,27 +17,122 @@ class GUI(BaseWidget):
         self._interest_cr = ControlText('Процентная ставка')
         self._period_cr = ControlText('Срок кредитования')
         self._overpay_cr = ControlText('Сумма переплат')
+        self._payment_cr = ControlText('Ежемесячный платеж')
         self._table_cr = ControlTextArea('График платежей')
         self._button_cr = ControlButton('Рассчитать')
+        self._button_save_cr = ControlButton('Сохранить отчёт')
+        self._button_cr.value = self.__buttonAction_cr
+        self._button_save_cr.value = self.__buttonSave_cr
 
-        self._amount_rs  = ControlText('Сумма кредита')
-        self._interest_rs = ControlText('Процентная ставка')
-        self._period_rs = ControlText('Срок кредитования')
-        self._overpay_rs = ControlText('Сумма переплат')
-        self._table_rs = ControlTextArea('График платежей')
-        self._button_rs = ControlButton('Рассчитать')
+        self._amount_ins  = ControlText('Сумма кредита')
+        self._interest_ins = ControlText('Процентная ставка')
+        self._period_ins = ControlText('Срок кредитования')
+        self._overpay_ins = ControlText('Сумма переплат')
+        self._payment_ins = ControlText('Средний платеж в месяц')
+        self._table_ins = ControlTextArea('График платежей')
+        self._button_ins = ControlButton('Рассчитать')
+        self._button_save_ins = ControlButton('Сохранить отчёт')
+        self._button_ins.value = self.__buttonAction_ins
+        self._button_save_ins.value = self.__buttonSave_ins
 
-        #Define the button action
-        self._button_cr.value = self.__buttonAction_credit
-        self._button_rs.value = self.__buttonAction_rs
+        self._amount_dep  = ControlText('Сумма вклада')
+        self._period_dep = ControlText('Срок размещения')
+        self._interest_dep = ControlText('Процентная ставка')
+        self._additions_dep = ControlCombo('Пополнение вклада')
+        self._overpay_dep = ControlText('Доход')
+        self._payment_dep = ControlText('Сумма в конце срока')
+        self._table_dep = ControlTextArea('График начислений')
+        self._additions_dep.add_item('Не предусмотрено')
+        self._additions_dep.add_item('Каждый месяц')
+        self._additions_dep.add_item('Каждый квартал')
+        self._additions_dep.add_item('Каждый год')
+        self._interest_type_dep = ControlCheckBox('Добавлять начисления ко вкладу')
+        self._amount_of_additions = ControlText('Сумма пополнений вклада')
+        self._button_dep = ControlButton('Рассчитать')
+        self._button_save_dep = ControlButton('Сохранить отчёт')
+        self._button_dep.value = self.__buttonAction_dep
+        self._button_save_dep.value = self.__buttonSave_dep
+
+
         self.formset = [ {
-        'Кредит':['_amount_cr', '=', '_interest_cr', '=', '_period_cr', '=', '_overpay_cr', '=', '_table_cr', '=','_button_cr', '='],
-        'Рассрочка':['_amount_rs', '=', '_interest_rs', '=', '_period_rs', '=', '_overpay_rs', '=', '_table_rs', '=', '_button_rs', '=']
+        'Кредит':['_amount_cr', '||', '_interest_cr', '||', '_period_cr', '=', ' ', '_overpay_cr', '=', '_payment_cr', '=', '_table_cr', '=','_button_cr', '=', '_button_save_cr', '='],
+        'Рассрочка':['_amount_ins', '||', '_interest_ins', '||', '_period_ins', '=', ' ', '_overpay_ins', '=', '_payment_ins', '=', '_table_ins', '=', '_button_ins', '=', '_button_save_ins', '='],
+        'Вклад':['_amount_dep', '||', '_period_dep', '||', '_interest_dep', '=', '_additions_dep', '=', '_interest_type_dep', '||', '_amount_of_additions', '=', '_overpay_dep', '||', '_payment_dep', '=', '_table_dep', '=', '_button_dep', '=', '_button_save_dep']    
             },
         ]
+
+        self.copy_num_cr = 0
+        self.copy_num_ins = 0
+        self.copy_num_dep = 0
+
+
+
+    def __buttonAction_dep(self):
+        interest_type = self._interest_type_dep.value
+        amount_of_additions = self._amount_of_additions.value
+        additions = self._additions_dep.value
+        amount_str = self._amount_dep.value
+        interest_str = self._interest_dep.value
+        period_str = self._period_dep.value
         
-    def __buttonAction_credit(self):
-        """Button action event"""
+        if amount_str == '' or not interest_str or not period_str:
+            self._table_dep.value = 'Error: Please enter all values'
+            return
+        elif self._additions_dep.value != 'Не предусмотрено':
+            if self._amount_of_additions.value == '':
+                self._table_dep.value = 'Error: Please enter all values'
+                return
+
+        deposit = Deposit(float(amount_str), interest_str, period_str, interest_type, additions, amount_of_additions)
+        data = deposit.activate()
+        data_str = '№         Дата платежа                Начислено процентов               Пополнение вклада               Остаток вклада\n'
+        self._table_dep.value = ''
+        for i in range(0, len(data[0])):
+            for j in range(0, len(data[0][i])):
+                data_str += ''.join(str(data[0][i][j][1])) + \
+                            ''.join(map(lambda _: ' ', range(25 + int(len(str(data[0][i][j][1])) * 1.2))))
+            self._table_dep += data_str + '\n'
+            data_str = ''
+
+        self._overpay_dep.value = str(data[1][1][1])
+        self._payment_dep.value = str(data[1][0][1])
+
+    def __buttonSave_dep(self):
+        amount_str = float(self._amount_dep.value)
+        interest_str = self._interest_dep.value
+        period_str = self._period_dep.value
+
+        if amount_str == '' or not interest_str or not period_str:
+            self._table_dep.value = 'Error: Please enter all values'
+            return
+        elif self._additions_dep.value != 'Не предусмотрено':
+            if self._amount_of_additions.value == '':
+                self._table_dep.value = 'Error: Please enter all values'
+                return
+        
+        deposit = Deposit(float(amount_str), interest_str, period_str, self._interest_type_dep.value, self._additions_dep.value, self._amount_of_additions.value)
+        data_lst, compute_list = deposit.activate()
+        deposit.save_docx(data_lst, compute_list, self.copy_num_dep)
+        self.copy_num_dep += 1
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def __buttonAction_cr(self):
         amount_str = float(self._amount_cr.value)
         interest_str = self._interest_cr.value
         period_str = self._period_cr.value
@@ -47,38 +144,78 @@ class GUI(BaseWidget):
             return
 
         data = credit.activate()
-        data_str = ''
+        data_str = '№                   Дата платежа                Платёж в месяц              Основной долг              Проценты                 Остаток\n'
+        self._table_cr.value = ''
         for i in range(0, len(data[0])):
             for j in range(0, len(data[0][i])):
-                for k in range(0, len(data[0][i][j])):
-                    data_str += "".join(str(data[0][i][j][k])) + '    '
+                data_str += ''.join(str(data[0][i][j][1])) + \
+                            ''.join(map(lambda _: ' ', range(15 + int(len(str(data[0][i][j][1])) * 1.2))))
             self._table_cr += data_str + '\n'
             data_str = ''
 
         self._overpay_cr.value = str(data[1][1][1])
+        self._payment_cr.value = str(data[1][0][1])
 
-
-    def __buttonAction_rs(self):
-        """Button action event"""
-        amount_str = float(self._amount_rs.value)
-        interest_str = self._interest_rs.value
-        period_str = self._period_rs.value
+    def __buttonSave_cr(self):
+        amount_str = float(self._amount_cr.value)
+        interest_str = self._interest_cr.value
+        period_str = self._period_cr.value
 
         credit = Credit(amount_str, interest_str, period_str)
-        
+
         if not amount_str or not interest_str or not period_str or not credit.check_input():
             self._table_cr.value = 'Error: Please enter all values'
             return
+        
+        data_lst = credit.activate()[0]
+        credit.save_docx(data_lst, self.copy_num_cr)
+        self.copy_num_cr += 1
 
-        data = credit.activate()
-        data_str = ''
+
+
+
+    def __buttonAction_ins(self):
+        amount_str = float(self._amount_ins.value)
+        interest_str = self._interest_ins.value
+        period_str = self._period_ins.value
+
+        installment = Installment(amount_str, interest_str, period_str)
+        
+        if not amount_str or not interest_str or not period_str or not installment.check_input():
+            self._table_ins.value = 'Error: Please enter all values'
+            return
+
+        data = installment.activate()
+        data_str = '№       Дата платежа    Платёж в месяц      Основной долг     Проценты       Остаток\n'
+        self._table_ins.value = ''
         for i in range(0, len(data[0])):
             for j in range(0, len(data[0][i])):
-                for k in range(0, len(data[0][i][j])):
-                    data_str += "".join(str(data[0][i][j][k])) + '    '
-            self._table_rs += data_str + '\n'
+                data_str += ''.join(str(data[0][i][j][1])) + \
+                            ''.join(map(lambda _: ' ', range(15 + int(len(str(data[0][i][j][1])) * 1.2))))
+            self._table_ins += data_str + '\n'
             data_str = ''
 
-        self._overpay_rs.value = str(data[1][1][1])
+        self._overpay_ins.value = str(data[1][1][1])
+        self._payment_ins.value = str(data[1][0][1])
+
+
+    def __buttonSave_ins(self):
+        amount_str = float(self._amount_ins.value)
+        interest_str = self._interest_ins.value
+        period_str = self._period_ins.value
+
+        installment = Installment(amount_str, interest_str, period_str)
+
+        if not amount_str or not interest_str or not period_str or not installment.check_input():
+            self._table_ins.value = 'Error: Please enter all values'
+            return
+        
+        data_lst = installment.activate()[0]
+        installment.save_docx(data_lst, self.copy_num_ins)
+        self.copy_num_ins += 1
+
+
+
+
 
 if __name__ == "__main__": pyforms.start_app( GUI )
